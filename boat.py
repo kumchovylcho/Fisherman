540f97948fd27585558679e74616cea7f662e569
petko <p.todorov1015@gmail.com>
2023-01-13 11:21:06 +0200

    boat methods (improved)

--- boat.py
+++ boat.py
@@ -2,7 +2,9 @@ import pygame
 
 
 class Boat:
+    # move_speed = 3
     move_speed = 3
+    move_horizontal_due_waves = 0.18
 
     def __init__(self, x=700, y=100):
         self.x = x
@@ -14,8 +16,18 @@ class Boat:
         boat_right = pygame.image.load("images/boat_right.png")
         return boat_left, boat_right
 
-    def move_left(self):
-        self.x -= Boat.move_speed
+    def move_left(self, second_passed):
+        if self.x > -10:
+            self.x -= Boat.move_speed
+            if second_passed % 2 == 0:
+                self.y -= Boat.move_horizontal_due_waves
+            else:
+                self.y += Boat.move_horizontal_due_waves
 
-    def move_right(self):
-        self.x += Boat.move_speed
+    def move_right(self, second_passed):
+        if self.x < 1450:
+            self.x += Boat.move_speed
+            if second_passed % 2 == 0:
+                self.y -= Boat.move_horizontal_due_waves
+            else:
+                self.y += Boat.move_horizontal_due_waves
