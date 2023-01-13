d3e99beb71b00a16a617d6e1799fdf0e5ec6cfae
kumchovylcho <tamerchoyy@gmail.com>
2023-01-13 23:20:44 +0200

    refactored variables and fixed left and right wall collisions

--- boat.py
+++ boat.py
@@ -2,33 +2,35 @@ import pygame
 
 
 class Boat:
-    move_speed = 10
-    move_horizontal_due_waves = 0.18
+    __MOVE_SPEED = 10
+    __MOVE_HORIZONTAL_DUE_WAVES = 0.18
+    __BOAT_WIDTH = 250
+    __BOAT_HEIGHT = 130
 
-    def __init__(self, x=700, y=69):
+    def __init__(self, x=700, y=50):
         self.x = x
         self.y = y
 
     @staticmethod
     def load_boat():
         boat_left = pygame.image.load("images/boat_left.png")
-        boat_left = pygame.transform.scale(boat_left, (300, 130))
+        boat_left = pygame.transform.scale(boat_left, (Boat.__BOAT_WIDTH, Boat.__BOAT_HEIGHT))
         boat_right = pygame.image.load("images/boat_right.png")
-        boat_right = pygame.transform.scale(boat_right, (300, 130))
+        boat_right = pygame.transform.scale(boat_right, (Boat.__BOAT_WIDTH, Boat.__BOAT_HEIGHT))
         return boat_left, boat_right
 
     def move_left(self, second_passed):
-        if self.x > -90:
-            self.x -= Boat.move_speed
+        if self.x > 0:
+            self.x -= Boat.__MOVE_SPEED
             if second_passed % 2 == 0:
-                self.y -= Boat.move_horizontal_due_waves
+                self.y -= Boat.__MOVE_HORIZONTAL_DUE_WAVES
             else:
-                self.y += Boat.move_horizontal_due_waves
+                self.y += Boat.__MOVE_HORIZONTAL_DUE_WAVES
 
-    def move_right(self, second_passed):
-        if self.x < 1390:
-            self.x += Boat.move_speed
+    def move_right(self, second_passed, screen_width: int):
+        if self.x < screen_width - Boat.__BOAT_WIDTH:
+            self.x += Boat.__MOVE_SPEED
             if second_passed % 2 == 0:
-                self.y -= Boat.move_horizontal_due_waves
+                self.y -= Boat.__MOVE_HORIZONTAL_DUE_WAVES
             else:
-                self.y += Boat.move_horizontal_due_waves
+                self.y += Boat.__MOVE_HORIZONTAL_DUE_WAVES
