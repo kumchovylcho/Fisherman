e0ee7e159668960b8736cff0486e6759df35d9db
kumchovylcho <tamerchoyy@gmail.com>
2023-01-14 02:39:51 +0200

    added fishing line, hook, and made drawing methods.All working properly. Collisions must be added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from boat import Boat
 from fish import Fish
 from fishing_line import FishLine
+from hook import Hook
 
 SIZE = (1600, 900)
 pygame.init()
@@ -10,15 +11,16 @@ screen = pygame.display.set_mode(SIZE)
 background = pygame.image.load("images/background.png")
 
 boat = Boat()
-left_picture_boat, right_picture_boat = boat.load_boat()
 fish = Fish(800, 500)
+fisherman_line = FishLine(boat)
+hook = Hook(boat)
+
+left_picture_boat, right_picture_boat = boat.load_boat()
 left_picture_fish, right_picture_fish = fish.load_pictures()
-fisherman_line = FishLine()
 
 boat_look_direction = left_picture_boat
 fish_look_direction = left_picture_fish if fish.x_pos <= SIZE[0] // 2 else right_picture_fish
 
-is_hook_moving = False
 running = True
 while running:
     pygame.time.Clock().tick(60)
@@ -28,14 +30,16 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                is_hook_moving = True
+                hook.is_hook_moving = True
 
     seconds = pygame.time.get_ticks() // 1000
-    if pygame.key.get_pressed()[pygame.K_a] and not is_hook_moving:
+    if pygame.key.get_pressed()[pygame.K_a] and not hook.is_hook_moving:
         boat.move_left(seconds)
+        fisherman_line.rotate_fisherman_left(boat)
         boat_look_direction = left_picture_boat
-    elif pygame.key.get_pressed()[pygame.K_d] and not is_hook_moving:
-        boat.move_right(seconds)
+    elif pygame.key.get_pressed()[pygame.K_d] and not hook.is_hook_moving:
+        boat.move_right(seconds, SIZE[0])
+        fisherman_line.rotate_fisherman_right(boat)
         boat_look_direction = right_picture_boat
 
     if fish_look_direction == left_picture_fish:
@@ -48,9 +52,32 @@ while running:
         if fish.check_right_wall(screen_width):
             fish_look_direction = left_picture_fish
 
+    fish_rect = pygame.Rect((fish.x_pos, fish.y_pos, 0, 0))
+    hook_rect = pygame.Rect((boat.x, boat.y, 0, 0))
+    line = pygame.Rect((fisherman_line.tip_of_the_rod, boat.y + 17, 3, fisherman_line.advance_line))
+
     screen.blit(background, (0, 0))
+    if not hook.is_hook_moving:
+        pygame.draw.rect(screen, (255, 0, 0), line)
+    else:
+        if not hook.bottom_reached:
+            hook.drop_hook()
+        elif hook.bottom_reached:
+            hook.get_hook_back(fisherman_line)
+        pygame.draw.line(screen, (255, 0, 0), (fisherman_line.tip_of_the_rod, boat.y + 17),
+                         (fisherman_line.tip_of_the_rod, hook.y_pos), 3)
+
     screen.blit(boat_look_direction, (boat.x, boat.y))
-    pygame.draw.line(screen, (255, 0, 0), fisherman_line.start_point, fisherman_line.end_point, 5)
     screen.blit(fish_look_direction, (fish.x_pos, fish.y_pos))
-    screen.blit(fisherman_line.picture, (500, 500))
-    pygame.display.update()
+
+    """
+    fisherman_line.tip_of_the_rod - 10 === hook knot position
+    
+    hook.y_pos if hook.is_hook_moving else fisherman_line.advance_line + 62
+    hook.y_pos is the dynamic value
+    fisherman_line.advance + 62 is static value
+    """
+    screen.blit(hook.picture, (fisherman_line.tip_of_the_rod - 10,
+                               hook.y_pos if hook.is_hook_moving else fisherman_line.advance_line + 62))
+
+    pygame.display.flip()
