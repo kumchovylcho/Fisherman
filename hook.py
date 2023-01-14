87ec0b62cc3564c713bf70723d7fe67eab2114c1
kumchovylcho <tamerchoyy@gmail.com>
2023-01-14 02:41:03 +0200

    made the hook class which can drop and drag the hook.Caught fish method has to be added

---
+++ hook.py
@@ -0,0 +1,28 @@
+import pygame
+from boat import Boat
+from fishing_line import FishLine
+
+
+class Hook:
+    __DROP_SPEED = 4
+    __GET_BACK_SPEED = 8
+    __CAUGHT_FISH_SPEED = 2
+
+    def __init__(self, boat: Boat):
+        self.picture = pygame.image.load("images/hook.png")
+        self.picture = pygame.transform.scale(self.picture, (15, 30))
+        self.y_pos = boat.y + 17
+        self.is_hook_moving = False
+        self.bottom_reached = False
+
+    def drop_hook(self):
+        self.y_pos += Hook.__DROP_SPEED
+        if self.y_pos >= 900:
+            self.bottom_reached = True
+
+    def get_hook_back(self, fishing_line: FishLine):
+        if self.y_pos >= fishing_line.advance_line:
+            self.y_pos -= Hook.__GET_BACK_SPEED
+        else:
+            self.is_hook_moving = False
+            self.bottom_reached = False
